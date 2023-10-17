dfc7f91074bc862a18a13094b2e1b2236e7d8c20
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-10-17 16:18:04 -0700

    okay

--- main.h
+++ main.h
@@ -7,12 +7,25 @@
 #include <stdlib.h>
 #include <limits.h>
 
+/**
+ * struc format - compile with the conversion specifiers for printf
+ * @ig: type char pointer of the spcifier
+ * @p: pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *ig;
+	int (*p)();
+	convert_match;
+}
+
 int _putchar(char c);
 int ourprint_int(va_list args);
 int ourprint_dec(va_list args);
 int ourprintf_bin(va_list val);
 unsigned int our_buf(char *buf, char c, unsigned int ibufp);
 int _printf(const char *format, ...);
-
 
 #endif
