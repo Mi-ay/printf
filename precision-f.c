eea9ee9dee20dc37cb9d47ac56bc3a4860396fe0
Mi-ay <okunlolafavour16@gmail.com>
2023-10-18 14:11:02 -0700

    precision-f.c

--- precision-f.c
+++ precision-f.c
@@ -10,21 +10,21 @@
 
 char *our_precision(char *s, params_t *params, va_list p)
 {
-        int int_number = 0;
+	int int_number = 0;
 
-        if (*s != '.')
-                return (s);
-        s++;
-        if (*s == '*')
-        {
-                int_number = va_arg(p, int);
-                s++;
-        }
-        else
-        {
-                while (_isdigit(*s))
-                        int_number = int_number * 10 + (*s++ - '0');
-        }
-        params->precision = int_number;
-        return (s);
+	if (*s != '.')
+		return (s);
+	s++;
+	if (*s == '*')
+	{
+		int_number = va_arg(p, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			int_number = int_number * 10 + (*s++ - '0');
+	}
+	params->precision = int_number;
+	return (s);
 }
